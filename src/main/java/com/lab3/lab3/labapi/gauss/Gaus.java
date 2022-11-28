e8954ce6f2e9b7a64e880e66853272c2e93cae89
poramental <poramental@mail.ru>
2022-11-28 11:07:52 +0300

    fix Gauss decide method.

--- src/main/java/com/lab3/lab3/labapi/gauss/Gaus.java
+++ src/main/java/com/lab3/lab3/labapi/gauss/Gaus.java
@@ -10,67 +10,46 @@ public class Gaus {
         int n = expr.getMatrixOfCoefficents().getVerticalLength();
         Matrix a = expr.getMatrixOfCoefficents();
         Matrix y = expr.getResponseMatrix();
-        double max;
-        int k, index;
-        final double eps = 0.00001;  // точность
+        int k;
         double[] x = new double[n];
+        double s = 0;
         k = 0;
         while (k < n) 
         {
-            // Поиск строки с максимальным a[i][k]
-            max = Math.abs(a.getElement(k, k));
-            index = k;
-            for (int i = k + 1; i < n; i++) 
-            {
-                if (Math.abs(a.getElement(i, k)) > max)
-                {
-                    max = Math.abs(a.getElement(i, k));
-                    index = i;
+            y.setElement(0,k,y.getElement(0,k) / a.getElement(k, k));
+
+            for(int j = k + 1; j < n; j++){
+                    a.setElement(k, j, a.getElement(k, j) / a.getElement(k, k) );
+                System.out.println("ds");
                 }
-            }
-            // Перестановка строк
-            if (max < eps) 
-            {
-                // нет ненулевых диагональных элементов
-                throw new GaussException("нет ненулевых диагональных элементов.");
+        
             
-            }
-            for (int j = 0; j < n; j++) 
-            {
-                double temp = a.getElement(k, j);
-                a.setElement(k, j, a.getElement(index, j));
-                a.setElement(index, j, temp);
-            }
-            double temp = y.getElement(0, k);
-            y.setElement(0, k, y.getElement(0, index));
-            y.setElement(0, index, temp);
-            // Нормализация уравнений
-            for (int i = k; i < n; i++) 
-            {
-                double _temp = a.getElement(i, k);
-                if (Math.abs(temp) < eps) continue; // для нулевого коэффициента пропустить
-                for (int j = 0; j < n; j++) 
-                    a.setElement(i, j, a.getElement(i, j)/ _temp); 
-                y.setElement(0, i, a.getElement(0, i)/ _temp);
-                if (i == k)  continue; // уравнение не вычитать само из себя
-                for (int j = 0; j < n; j++){
-                    a.setElement(i, j, a.getElement(i, j)- a.getElement(k, j));
+            for(int i = k + 1; i < n ; i++ ){
+                y.setElement(0, i, y.getElement(0,i) - a.getElement(i, k)*y.getElement(0, k));
+                for(int j = k + 1;j < n; j++ ){
+                    a.setElement(i, j, a.getElement(i, j) - a.getElement(i, k)*a.getElement(k, j));
+
                 }
-                y.setElement(0, i, y.getElement(0, i) - y.getElement(0, k)) ;
-                // y[i] = y[i] - y[k];
+
             }
+
             k++;
         }
-        // обратная подстановка
-        for (k = n - 1; k >= 0; k--)
-        {
-            x[k] = y.getElement(0, k);
-            for (int i = 0; i < k; i++){
-                y.setElement(0,i,y.getElement(0, i) - a.getElement(i, k) * x[k]);
+
+        for(int i = 0; i < y.getHorizontalLength(); i++){
+            x[i] = y.getElement(0, i);
+        }
+
+        for(int i = n - 1; i != 0;  i--){
+            for(int j = k + 1; j < n;j++){
+                s = a.getElement(i, j)*x[k];                
             }
+           x[i] = y.getElement(0,i) - s;
             
         }
+
         return x;
-        }
+
     }
 
+}
